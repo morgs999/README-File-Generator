bdcb609092fd0732e2e9c415890c4c446cc18ef4
Morgan <morgs99@gmail.com>
2023-12-06 16:31:34 -0500

    inquirer started

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What would you like the title of your README to be: ',
+            },
+            {
+                type: 'editor',
+                name: 'description',
+                message: 'Please enter a description for your README file: ',
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'How do you install your app? ',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'How do you use your app? ',
+            },
+            {
+                type: 'input',
+                name: 'contribute',
+                message: 'How can someone contribute to your code?',
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'How can someone test your code?',
+            },
+            {
+                type: 'checkbox',
+                name: 'license',
+                message: 'Which license would you like to use?',
+                choices: ['MIT', 'Createive Commons', 'Mozilla', 'Hippocratic']
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: 'What is your github username?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email address?',
+            }
+        ])
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
